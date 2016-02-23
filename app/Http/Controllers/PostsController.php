153c12cb7899176f4647b2c42e9cf595fe101650
Renee Dudley <reneedudley@Renees-MacBook-Pro.local>
2016-02-23 15:37:13 -0600

    controllers

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \App\Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new \App\Post;
+        $post->user_id = $request->user_id;
+        $post->content = $request->content;
+        $post->save();
+
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return \App\Post::with('users')->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = \App\Post::find($id);
+        $post = new \App\Post;
+        $post->user_id = $request->user_id;
+        $post->content = $request->content;
+        $post->save();
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = \App\Post::destroy($id);
+
+    }
+}
