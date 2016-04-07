fcbfe54c1090f777b37652891f83d5b5dcabb99f
reneedudley <4reneedudley@gmail.com>
2016-04-07 14:25:21 -0500

    fucking a it works

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
 'use strict';
+var $ = window.$ = window.jQuery = require('jquery');
 
 var $ = require('jquery');
 var Backbone = require('backbone');
 var HomeView = require('./views/HomeView.js');
-
+var $ = window.$ = window.jQuery = require('jquery');
 $(function($){
 	$.ajaxSetup({
 	headers: {
--- public/js/views/HomeView.js
+++ public/js/views/HomeView.js
@@ -4,10 +4,12 @@ var Backbone = require('backbone');
 var PostsListView = require('./PostsListView.js');
 var PostsCollection = require('../collections/PostsCollection.js');
 
+
+
 var HomeView = Backbone.View.extend({
 	el: '\
 	<div class="container">\
-		<div> class="row">\
+		<div class="row">\
 			<div class="eight column" id="allposts">\
 			</div>\
 		</div>\
